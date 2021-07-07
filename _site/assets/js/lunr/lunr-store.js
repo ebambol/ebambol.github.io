e8bdd4bbb7faee040116eb9fa277f2666818b75b
ebambol <pale0865@gmail.com>
2021-07-07 21:10:05 +0900

    edit post

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "도시락드림",
+        "excerpt":"도시락드림ppt 를 정리한 페이지입니다. ppt는 Github에서 받으실 수 있습니다. 🌲 프로젝트 개요 시니어 세대를 위한 도시락 배달 및 도움 서비스 상품의 주문도 받고 고령자의 안부를 확인하여 가족이 안심할 수 있는 도시락 제품을 판매하기 위한 플랫폼 개발하였습니다. 소스코드 Github 🍁 개발환경 Java 1.8.0_171 Tomcat 9.0 Bootstrap 5.0 beta MySQL 8.0.17 🍁...","categories": ["Project"],
+        "tags": ["Project","DosirakDream"],
+        "url": "/project/DosirakDream/",
+        "teaser": null
+      },{
+        "title": "Maven",
+        "excerpt":"💡 이 글은 뉴렉처 Maven강의를 듣고 복습을 목적으로 정리한 글입니다. 🍀maven 자바 빌드를 도와주는 빌드툴이다. Gradle, Ant 등이 있다. 🌲 빌드 과정 프로젝트 생성과 라이브러리설정 작업을 하는 것을 빌드툴이라고 한다. 🌲 설치 방법 Apache Maven에 접속하여 Binary zip archive을 다운받고 적당한 위치에 압축을 푼다. 환경변수를 설정한다. %M2_HOME%에 설치경로를 작성하고, path에...","categories": ["Java"],
+        "tags": ["Maven"],
+        "url": "/java/maven/",
+        "teaser": null
+      },{
+        "title": "Network 흐름만 알아보기",
+        "excerpt":"실제로는 더 복잡한 과정이지만 많이 생략되어 있습니다. 아래 이미지 파일을 다른 곳으로 전송하고자 할 때 컴퓨터는 어떻게 할까? 레이어가 전송할 수 있는 최대 프로토콜 데이터 단위인 프로토콜의 최대 전송 단위 (MTU; Maximum Transmission Unit)로 잘라 전송한다. 컴퓨터는 이진법만을 알아듣는데, 만약 이미지 용량이 1MB이면 1048576byte, 1바이트는 8bit이므로 8388608bit, 약 838만 개의...","categories": ["Network"],
+        "tags": ["Computer Science"],
+        "url": "/network/Internet/",
+        "teaser": null
+      }]
